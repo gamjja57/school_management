60f9c9cf99f6742c935e8335dc98b5845a799b3e
gamjja57 <dldmswl12345@gmail.com>
2021-12-13 14:44:27 +0900

    추가, 삭제 기능 added

---
+++ src/main/java/com/greenart/school_management/mapper/DepartmentMapper.java
@@ -0,0 +1,17 @@
+package com.greenart.school_management.mapper;
+
+import java.util.List;
+
+import com.greenart.school_management.data.DepartmentVO;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface DepartmentMapper {
+    public List<DepartmentVO> getDepartmentInfo(Integer offset);
+    public Integer getDepartmentCount();
+
+    public void addDeprtment(DepartmentVO data);
+
+    public void deleteDepartment(Integer seq);
+}
